ba2b3e630fbdb59eb0acb30530d3977c4e822708
harshv1741 <harshv1741@gmail.com>
2023-04-15 16:16:03 +0530

    Photos size is solved

--- public/scripts/photos.js
+++ public/scripts/photos.js
@@ -44,6 +44,8 @@ function viewPhoto(fileName) {
 			// Set the src attribute of the img element to the download URL
 			var img = document.createElement("img");
 			img.src = url;
+			img.style.maxWidth = "200px"; // Set max width to 200px
+			img.style.maxHeight = "200px"; // Set max height to 200px
 			photoDisplay.innerHTML = ""; // Clear previous image display
 			photoDisplay.appendChild(img);
 		})
@@ -70,6 +72,19 @@ function addPhotoToList(fileName) {
 	listItem.appendChild(deleteButton);
 	photoList.appendChild(listItem);
 }
+
+// Fetch and display all photos from storage on page load
+storageRef
+	.listAll()
+	.then(function (res) {
+		res.items.forEach(function (item) {
+			var fileName = item.name;
+			addPhotoToList(fileName); // Add all photo names to the list on page load
+		});
+	})
+	.catch(function (error) {
+		console.error("Failed to fetch files from storage:", error);
+	});
 
 // Remove photo from the list
 function removePhotoFromList(fileName) {
