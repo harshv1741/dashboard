b44ca869a60c156a76825c6cff220b295f8730f9
harshv1741 <harshv1741@gmail.com>
2023-04-20 11:52:49 +0530

    Adding the data from the project

--- public/scripts/transaction.js
+++ public/scripts/transaction.js
@@ -1,9 +1,12 @@
 // // JavaScript code
+// const transactionsRef = db.collection("transactions");
+
 // const table = document.querySelector("table");
 // const tbody = table.querySelector("tbody");
 // const totalRow = table.querySelector(".total-row");
 // const totalCell = totalRow.querySelector(".total");
 // const addRowButton = document.querySelector(".add-row-button");
+// const deleteRowButton = document.querySelector(".delete-row-button");
 // const formContainer = document.querySelector(".form-container");
 // const form = formContainer.querySelector("form");
 // const cancelButton = formContainer.querySelector(".cancel-button");
@@ -13,41 +16,52 @@
 // function render() {
 // 	tbody.innerHTML = "";
 
-// 	for (let i = 0; i < transactions.length; i++) {
-// 		const transaction = transactions[i];
-// 		const row = document.createElement("tr");
+// 	transactionsRef.get().then((querySnapshot) => {
+// 		querySnapshot.forEach((doc) => {
+// 			const transaction = doc.data();
+// 			const row = document.createElement("tr");
 
-// 		const dateCell = document.createElement("td");
-// 		dateCell.textContent = transaction.date;
-// 		row.appendChild(dateCell);
+// 			const dateCell = document.createElement("td");
+// 			dateCell.textContent = transaction.date;
+// 			row.appendChild(dateCell);
 
-// 		const descriptionCell = document.createElement("td");
-// 		descriptionCell.textContent = transaction.description;
-// 		row.appendChild(descriptionCell);
+// 			const descriptionCell = document.createElement("td");
+// 			descriptionCell.textContent = transaction.description;
+// 			row.appendChild(descriptionCell);
 
-// 		const priceCell = document.createElement("td");
-// 		const priceText = transaction.price.toLocaleString("en-IN", {
+// 			const priceCell = document.createElement("td");
+// 			const priceText = transaction.price.toLocaleString("en-IN", {
+// 				style: "currency",
+// 				currency: "INR",
+// 			});
+// 			priceCell.textContent = priceText;
+// 			row.appendChild(priceCell);
+
+// 			tbody.appendChild(row);
+// 		});
+
+// 		const total = transactions.reduce((acc, cur) => acc + cur.price, 0);
+// 		const totalText = total.toLocaleString("en-IN", {
 // 			style: "currency",
 // 			currency: "INR",
 // 		});
-// 		priceCell.textContent = priceText;
-// 		row.appendChild(priceCell);
-
-// 		tbody.appendChild(row);
-// 	}
-
-// 	const total = transactions.reduce((acc, cur) => acc + cur.price, 0);
-// 	const totalText = total.toLocaleString("en-IN", {
-// 		style: "currency",
-// 		currency: "INR",
+// 		totalCell.textContent = totalText;
 // 	});
-// 	totalCell.textContent = totalText;
 // }
 
 // function addTransaction(date, description, price) {
-// 	const transaction = { date, description, price };
-// 	transactions.push(transaction);
-// 	render();
+// 	transactionsRef
+// 		.add({
+// 			date: date,
+// 			description: description,
+// 			price: price,
+// 		})
+// 		.then(() => {
+// 			console.log("Transaction added successfully!");
+// 		})
+// 		.catch((error) => {
+// 			console.error("Error adding transaction: ", error);
+// 		});
 // }
 
 // function handleSubmit(event) {
@@ -72,7 +86,6 @@
 // form.addEventListener("submit", handleSubmit);
 // render();
 
-// JavaScript code
 const transactionsRef = db.collection("transactions");
 
 const table = document.querySelector("table");
@@ -109,6 +122,25 @@ function render() {
 			});
 			priceCell.textContent = priceText;
 			row.appendChild(priceCell);
+
+			const deleteCell = document.createElement("td");
+			const deleteButton = document.createElement("button");
+			deleteButton.classList.add("delete-row-button");
+			deleteButton.textContent = "Delete";
+			deleteButton.addEventListener("click", () => {
+				transactionsRef
+					.doc(doc.id)
+					.delete()
+					.then(() => {
+						console.log("Transaction deleted successfully!");
+					})
+					.catch((error) => {
+						console.error("Error deleting transaction: ", error);
+					});
+				row.remove();
+			});
+			deleteCell.appendChild(deleteButton);
+			row.appendChild(deleteCell);
 
 			tbody.appendChild(row);
 		});
