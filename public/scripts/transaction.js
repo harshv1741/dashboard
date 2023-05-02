2110796a6152aad6bb8255ffbf11bb09d2fdbc71
harshv1741 <harshv1741@gmail.com>
2023-05-02 12:35:01 +0530

    Reverting Back Photos Page

--- public/scripts/transaction.js
+++ public/scripts/transaction.js
@@ -124,7 +124,7 @@ function addTransaction(date, description, price) {
 			console.error("Error adding transaction: ", error);
 		});
 
-	setTimeout(() => window.location.reload(), 1000);
+	setTimeout(() => window.location.reload(), 4000);
 }
 
 function handleSubmit(event) {
